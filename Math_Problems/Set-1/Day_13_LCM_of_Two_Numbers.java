28b2f61fe0e1c62573cec87acdfbf8d951319ad8
Suniksha <sunikshabenpatel@gmail.com>
2024-06-11 18:05:45 +0530

    Math Problem

--- Math_Problems/Set-1/Day_13_LCM_of_Two_Numbers.java
+++ Math_Problems/Set-1/Day_13_LCM_of_Two_Numbers.java
@@ -25,4 +25,29 @@ public class Day_13_LCM_of_Two_Numbers {
     }
 } */
 
-/*  */
+/* Next we can solve using conditional for loop with 
+ * Time Complexity : O(min(a,b))
+ * Space Complexity : O(1)
+ */
+
+import java.util.*;
+public class Day_13_LCM_of_Two_Numbers {
+    public static int findLCM(int a , int b) {
+        int greater = Math.max(a,b);
+        int smallest = Math.min(a,b);
+        for(int i = greater;;i += greater) {
+            if(i % smallest == 0)
+               return i;
+        }
+
+    }
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int a = in.nextInt();
+        System.out.print("Enter a Number : ");
+        int b = in.nextInt();
+        int lcm = findLCM(a, b);
+        System.out.print(lcm);
+    }
+}
