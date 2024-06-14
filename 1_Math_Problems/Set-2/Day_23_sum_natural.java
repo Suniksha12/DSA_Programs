572758fe00fb4e59ae4093ed2b4571f8a6fc1ea9
Suniksha <sunikshabenpatel@gmail.com>
2024-06-14 10:04:41 +0530

    Math Problem

--- 1_Math_Problems/Set-2/Day_23_sum_natural.java
+++ 1_Math_Problems/Set-2/Day_23_sum_natural.java
@@ -8,6 +8,37 @@
  * Space Complexity : O(1)
  */
 
+/*import java.util.*;
 public class Day_23_sum_natural {
-    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a Number: ");
+        int N = in.nextInt();
+        int sum =0;
+        for(int i =1;i<=N;i++) {
+            sum += i;
+        }
+        System.out.println("Sum of First " + N + " Natural Number = " + sum);
+        in.close();
+    }
+} */
+
+/* Approach -2 : Using Mathematical Functions
+ * Sum = N*(N+1)/2
+ * 
+ * Time Complexity : O(1)
+ * Space Complexity : O(1)
+ *
+ */
+
+import java.util.*;
+public class Day_23_sum_natural {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a Number: ");
+        int N = in.nextInt();
+        int sum = (N*(N+1))/2;
+        System.out.print("Sum of First " + N + " Natural Numbers = " + sum);
+        in.close();
+    }
 }
--- 1_Math_Problems/Set-2/Day_24_number_elements_odd_factors.java
+++ 1_Math_Problems/Set-2/Day_24_number_elements_odd_factors.java
@@ -1,3 +1,31 @@
+//Implement Code in java
+
+/* Given a range [n,m], find the number of elements that have odd number of factors in the given range (n and m inclusive). 
+Examples : 
+ 
+
+Input  : n = 5, m = 100
+Output : 8
+The numbers with odd factors are 9, 16, 25, 
+36, 49, 64, 81 and 100 
+
+Approach Used - An Efficient Solution is to observe the pattern. Only those numbers, which are perfect Squares have an odd number of factors.
+
+Time Complexity : O(1)
+Space compelxity : O(1)*/
+
+import java.util.*;
 public class Day_24_number_elements_odd_factors {
-    
+    static int perfectoddsquares(int n , int m) {
+        return (int)Math.pow((double)m,0.5) - (int)Math.pow((double)n-1,0.5);
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a Number : ");
+        int n = in.nextInt();
+        System.out.print("Enter a Number : ");
+        int m = in.nextInt();
+        System.out.print("Count is " + perfectoddsquares(n, m));
+        in.close();
+    }
 }
