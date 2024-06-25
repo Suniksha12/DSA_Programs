2a43b6d45b7e26007da5f313f2baa0b2fe9f4f68
Suniksha <sunikshabenpatel@gmail.com>
2024-06-25 19:32:03 +0530

    Arrays&Strings

---
+++ 2_Arrays&Strings/Set - 1/P09_Validate_IP_Address.java
@@ -0,0 +1,16 @@
+/* Implement a program to validate an IPv4 Address.
+ * 
+ * According to Wikipedia, IPv4 addresses are canonically represented in 
+ * dot-decimal notation, which consists of four decimal numbers, each ranging from 0 to 255,
+ *  separated by dots, e.g., 172.16.254.1
+ * 
+ * Approach 1 - Simple Approach
+ * 
+ * Time Complexity : O(n)
+ * Space Complexity : O(1)
+ */
+
+import java.util.*;
+public class P09_Validate_IP_Address {
+    
+}
