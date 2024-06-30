b7907007dbd5296358d34808fd83557ca55390e8
Suniksha <sunikshabenpatel@gmail.com>
2024-06-30 11:04:32 +0530

    Arrays&Strings - SET 2

--- 2_Arrays&Strings/Set - 2/Long_subsequence.java
+++ 2_Arrays&Strings/Set - 2/Long_subsequence.java
@@ -12,7 +12,7 @@ Approach - 1 Naive Approach
 Time Complexity : O(N(log N))
 Space complexity : O(N)*/
 
-import java.util.*;
+/*import java.util.*;
 public class Long_subsequence {
     static int findlongestsubsequence(int arr[], int n) {
         Arrays.sort(arr);
@@ -42,5 +42,81 @@ public class Long_subsequence {
        }
        System.out.println(findlongestsubsequence(arr,n));
        in.close();
+    }
+} */
+
+/* Approach 2 : hashing 
+ Time Complexity : O(n)
+ Space Complexity : O(n)*/ 
+
+/*import java.util.*;
+public class Long_subsequence { 
+    static int findlongestsubsequence(int arr[], int n) {
+       HashSet<Integer> s = new HashSet<>();
+       int ans =0;
+       for(int i =0;i<n;++i)
+           s.add(arr[i]);
+
+       for(int i =0;i<n;++i){
+        if(!s.contains(arr[i] - 1)) {
+            int j = arr[i];
+            while(s.contains(j))
+                j++;
+
+            if(ans<j-arr[i]) 
+               ans = j - arr[i];
+        }
+       }
+       return ans;
+    }
+    public static void main(String[] args) {
+       
+        int arr[] = { 1, 9, 3, 10, 4, 20, 2 };
+        int n = arr.length;
+        System.out.println(
+            "Length of the Longest consecutive subsequence is "
+            + findlongestsubsequence(arr, n)); 
+    }
+} */
+
+/* Approach - 3 Priority Queue
+ * Time Complexity : O(N*log(N))
+ * Space Complexity : O(N)
+ */
+
+import java.util.*;
+public class Long_subsequence { 
+    static int findlongestsubsequence(int arr[] , int N) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i =0;i<N;i++) {
+            pq.add(arr[i]);
+        }
+        int prev = pq.poll();
+        int c =1;
+        int max = 1;
+        for(int i = 1;i<N;i++) {
+            if(pq.peek() - prev > 1) {
+                c = 1;
+                prev = pq.poll();
+            }
+            else if(pq.peek() - prev == 0) {
+                prev = pq.poll();
+            }
+            else {
+                c++;
+                prev = pq.poll();
+            }
+            if(max < c) {
+                max = c;
+            }
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        int arr[] = { 1, 9, 3, 10, 4, 20, 2 };
+        int n = arr.length;
+        System.out.println(
+            "Length of the Longest consecutive subsequence is "
+            + findlongestsubsequence(arr, n));
     }
 }
