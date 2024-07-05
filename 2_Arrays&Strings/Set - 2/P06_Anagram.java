6791e696e5ec6deaeb5e3c8b2fc4373ea498f6b5
Suniksha <sunikshabenpatel@gmail.com>
2024-07-05 22:51:48 +0530

    Arrays&Strings - SET 2

--- 2_Arrays&Strings/Set - 2/P06_Anagram.java
+++ 2_Arrays&Strings/Set - 2/P06_Anagram.java
@@ -14,6 +14,7 @@ Approach - 1 Sorting
 Time Compelxity : O(N * logN)
 Space Complexity : O(1)*/
 
+/*
 import java.util.Arrays;
 
 public class P06_Anagram {
@@ -38,6 +39,47 @@ public class P06_Anagram {
         char str2[] = { 'a', 'r', 'm' };
        
         // Function Call
+        if (areAnagram(str1, str2))
+            System.out.println("The two strings are"
+                               + " anagram of each other");
+        else
+            System.out.println("The two strings are not"
+                               + " anagram of each other");
+    }
+}
+*/
+
+/* Approach 2 - Frequency Count */
+
+import java.util.Arrays;
+
+public class P06_Anagram {
+    static int NO_OF_CHAR= 256;
+    static boolean areAnagram(char str1[], char str2[]) {
+        int count1[] = new int[NO_OF_CHAR];
+        Arrays.fill(count1,0);
+        int count2[] = new int[NO_OF_CHAR];
+        Arrays.fill(count2, 0);
+        int i;
+
+        for(i =0;i<str1.length && i < str2.length;i++) {
+            count1[str1[i]]++;
+            count2[str2[i]]++;
+        }
+        if(str1.length != str2.length)
+           return false;
+
+        for(i =0;i<NO_OF_CHAR;i++)
+            if(count1[i] != count2[i])
+               return false;
+
+        return true;
+    }
+    public static void main(String[] args) {
+        char str1[] = ("gram").toCharArray();
+        char str2[] = ("arm").toCharArray();
+ 
+        // Function call
         if (areAnagram(str1, str2))
             System.out.println("The two strings are"
                                + " anagram of each other");
