23d352d5250b06f229e1680081b299e5e53d2517
Suniksha <sunikshabenpatel@gmail.com>
2024-07-11 02:19:38 +0530

    BITWISE - SET 1

---
+++ 3_Bitwise_Operations/Set-1/P05_Help_Classmates.java
@@ -0,0 +1,51 @@
+/* Implement Code in java such that we Professor X wants his students to help each other in the chemistry lab. He suggests that every student should help out a classmate who scored less marks than him in chemistry and whose roll number appears after him. But the students are lazy and they don't want to search too far. They each pick the first roll number after them that fits the criteria. Find the marks of the classmate that each student picks.
+Note: one student may be selected by multiple classmates.
+
+Example 1:
+
+Input: N = 5, arr[] = {3, 8, 5, 2, 25}
+Output: 2 5 2 -1 -1
+Explanation: 
+1. Roll number 1 has 3 marks. The first person 
+who has less marks than him is roll number 4, 
+who has 2 marks.
+2. Roll number 2 has 8 marks, he helps student 
+with 5 marks.
+3. Roll number 3 has 5 marks, he helps student 
+with 2 marks.
+4. Roll number 4 and 5 can not pick anyone as 
+no student with higher roll number has lesser 
+marks than them. This is denoted by -1.
+Output shows the marks of the weaker student that 
+each roll number helps in order. ie- 2,5,2,-1,-1
+
+Time Complexity : O(n)
+
+Space Complexity : O(n)*/
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class P05_Help_Classmates {
+    public static int[] help_classmate(int arr[], int n) 
+	{ 
+	    Stack<Integer> st = new Stack<>();
+	    int a[] = new int[n];
+	    Arrays.fill(a,-1);
+	    for(int i =0;i<n;i++) {
+	        while(!st.isEmpty() && arr[st.peek()] > arr[i]) {
+	            a[st.pop()] = arr[i];
+	        }
+	        st.push(i);
+	    }
+	    return a;
+	} 
+    public static void main(String args[]) {
+        Scanner in= new Scanner(System.in);
+        int[] arr = new int[]{3, 8, 5, 2, 25};
+        int n = arr.length;
+        System.out.println(help_classmate(arr, n));
+        in.close();
+    }
+}
