41c3e377901ad4d0185a72f8804b6cef0cd09b59
Suniksha <sunikshabenpatel@gmail.com>
2024-07-10 14:12:01 +0530

    BITWISE - SET 1

--- 3_Bitwise_Operations/Set-1/P04_Occurring_oddNumber.java
+++ 3_Bitwise_Operations/Set-1/P04_Occurring_oddNumber.java
@@ -43,6 +43,7 @@ Space Complexity : O(1) */
  * Space Complexity : O(n)
 */
 
+/* 
 import java.util.HashMap;
 
 public class P04_Occurring_oddNumber { 
@@ -68,4 +69,28 @@ public class P04_Occurring_oddNumber {
         int n = arr.length;
         System.out.println(getOddOccurences(arr, n));
     } 
+} */
+
+/* Approach - 3 : Bitwise XOR
+ * 
+ * Time Complexity : O(n)
+ * 
+ * Space Complexity : O(1)
+*/
+
+public class P04_Occurring_oddNumber { 
+    static int getOddOccurences(int ar[] , int ar_size) {
+        int i;
+        int res = 0;
+        for(i =0;i<ar_size;i++) {
+            res = res ^ ar[i];
+        }
+        return res;
+    }
+    public static void main(String[] args) 
+    {
+        int ar[] = new int[]{2, 3, 5, 4, 5, 2, 4, 3, 5, 2, 4, 4, 2};
+        int n = ar.length;
+        System.out.println(getOddOccurences(ar, n));
+    }
 }
