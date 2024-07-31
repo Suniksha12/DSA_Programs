772a329da25c0d7aa6101e72dc6dff3309483218
Suniksha <sunikshabenpatel@gmail.com>
2024-07-31 10:31:30 +0530

    RECURSION - SET 1

--- 4_Recursion/SET-1/P01_Number_of_islands.java
+++ 4_Recursion/SET-1/P01_Number_of_islands.java
@@ -57,5 +57,51 @@ public class P01_Number_of_islands {
 
 /*Approach 2 - Finding the nUmber of Islands using DFS 
  * 
- * Time Complexity : 
+ * Time Complexity : O(ROW x COL)
+ * 
+ * Space Complexity : O(ROW * COL) extra auxilary space for stack space.
 */
+
+public class P01_Number_of_islands {
+    static void DFS(int[][] M , int i , int j , int ROW, int COL) {
+        if(i<0 || j <0 || i >(ROW-1) || j>(COL - 1) || M[i][j] != 1) {
+            return;
+        }
+        if(M[i][j] == 1) {
+            M[i][j] = 0;
+            DFS(M, i+1, j, ROW, COL);
+            DFS(M, i-1, j, ROW, COL);
+            DFS(M, i, j+1, ROW, COL);
+            DFS(M, i, j-1, ROW, COL);
+            DFS(M, i+1, j+1, ROW, COL);
+            DFS(M, i-1, j-1, ROW, COL);
+            DFS(M, i+1, j-1, ROW, COL);
+            DFS(M, i-1, j+1, ROW, COL);
+        }
+
+    }
+    static int countIslands(int[][] M) {
+        int ROW = M.length;
+        int COL = M[0].length;
+        int count =0;
+        for(int i =0;i<ROW;i++) {
+            for(int j =0;j<COL;j++) {
+                if(M[i][j] == 1) {
+                    count++;
+                    DFS(M, i, j, ROW, COL);
+                }
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        int[][] M = { { 1, 1, 0, 0, 0 },
+                      { 0, 1, 0, 0, 1 },
+                      { 1, 0, 0, 1, 1 },
+                      { 0, 0, 0, 0, 0 },
+                      { 1, 0, 1, 0, 1 } };
+
+        System.out.print("Number of islands is: "
+                         + countIslands(M));
+    }
+}
