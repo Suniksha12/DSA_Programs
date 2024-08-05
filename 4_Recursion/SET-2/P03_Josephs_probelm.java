e95699ccc051502454137f805c33af33cc5e7c6b
Suniksha <sunikshabenpatel@gmail.com>
2024-08-05 14:16:35 +0530

    Recursion Set-2

--- 4_Recursion/SET-2/P03_Josephs_probelm.java
+++ 4_Recursion/SET-2/P03_Josephs_probelm.java
@@ -15,6 +15,7 @@ Approach 1 - Using List
 Time Complexity : O(N^2)
 Space Complexity : O(N)*/
 
+/*
 import java.util.ArrayList;
 import java.util.List;
 
@@ -41,3 +42,44 @@ public class P03_Josephs_probelm {
     }
     
 }
+*/
+
+/*Approach 2 - Recursive Approach
+ * 
+ * Time Complexity : O(N^2)
+ * Space Complexity : O(N)
+ */
+
+ public class P03_Josephs_probelm {
+    public static void main(String[] args) {
+        int n =14, k=2;
+        System.out.println(joseph(n,k));
+    }
+    public static int joseph(int n, int k) {
+        k--;
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = 1;
+        }
+        int cnt =0,cut =0,num =1;
+        while(cnt <(n-1)) {
+            while(num <= k) {
+                cut++;
+                cut = cut % n;
+                if(arr[cut] == 1) {
+                    num++;
+                }
+            }
+            num =1;
+            arr[cut] =0;
+            cnt++;
+            cut++;
+            cut = cut % n;
+            while(arr[cut] == 0) {
+                cut++;
+                cut = cut % n;
+            }
+        }
+        return cut + 1;
+    }
+ }
