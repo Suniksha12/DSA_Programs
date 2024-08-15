4e3950378b50815a77afa6647753b737603de19c
Suniksha <sunikshabenpatel@gmail.com>
2024-08-15 15:11:25 +0530

    Searching and Sorting - Set-1

--- 5_Searching_&_Sorting/P09_Binary_Array_sorting.java
+++ 5_Searching_&_Sorting/P09_Binary_Array_sorting.java
@@ -9,6 +9,7 @@ Explanation: The output is a sorted array of 0 and 1
 Time Complexity : O(N)
 Space Complexity : O(1)*/
 
+/* 
 public class P09_Binary_Array_sorting {
     static void sortBinaryArray(int a[] , int n){
         int j =-1;
@@ -28,5 +29,38 @@ public class P09_Binary_Array_sorting {
         sortBinaryArray(a,n);
         for (int i = 0; i < n; i++)
             System.out.print(a[i] + " "); 
+    }
+} */
+
+/*Approach 2- Two pointers approach 
+ * Time Complexity : O(N)
+ * Space Complexity : O(1)
+*/
+
+public class P09_Binary_Array_sorting {
+    static void sort(int A[] , int n){
+        int i=0;
+        int j =n-1;
+        while(i<j){
+            if(A[i] == 1 && A[j] == 0) {
+                int temp = A[i];
+                A[i] = A[j];
+                A[j] = temp;
+                i++;
+                j--;
+            }
+            else if (A[i]==0) {
+                i++;
+            }
+            else if(A[j] == 1) {
+                j--;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = { 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1 };
+        sort(arr, arr.length);
+        for(int i:arr)
+            System.out.print(i+" ");
     }
 }
