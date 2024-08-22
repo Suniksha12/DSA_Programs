5010922d1eaf7be59af3352879fea347894c93fe
Suniksha <sunikshabenpatel@gmail.com>
2024-08-22 21:59:18 +0530

    Searching and Sorting Set - 2

--- 5_Searching_&_Sorting/Set-2/P05_Count1sin_SortedBinaryArray.java
+++ 5_Searching_&_Sorting/Set-2/P05_Count1sin_SortedBinaryArray.java
@@ -37,6 +37,7 @@ public class P05_Count1sin_SortedBinaryArray {
  * Spaec complexity : O(Log(N))
 */
 
+/* 
 public class P05_Count1sin_SortedBinaryArray {
     int count(int arr[] , int low , int high){
         if(high >= low){
@@ -55,4 +56,37 @@ public static void main(String[] args) {
         System.out.println("Count of 1's in given array is "
                            + ob.count(arr, 0, n - 1));
 }
+}*/
+
+/*Approach-3 using Binary Search 
+ * 
+ * Time Complexity :O(Log(N))
+ * Space Comlexity : O(1)
+*/
+
+/*
+public class P05_Count1sin_SortedBinaryArray {
+    static int count(int arr[] , int n){
+        int low =0,high = n-1;
+        while(low<=high){
+            int mid = (low + high)/2;
+            if(arr[mid]<1) high = mid -1;
+            else if(arr[mid]>1) low = mid+1;
+            else {
+                if(mid==n-1 || arr[mid+1] != 1) return mid+1;
+                else low = mid +1;
+            }
+        }
+        return 0;
+    }
+public static void main(String[] args) {
+    
+    int arr[] = { 1, 1, 1, 1, 0, 0, 0 };
+    int n = arr.length;
+
+    System.out.println("Count of 1's in given array is "
+                       + count(arr, n));
 }
+} */
+
+/*Approach - 4 u */
