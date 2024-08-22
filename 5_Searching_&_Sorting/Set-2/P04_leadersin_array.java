f9dd3a029a96f19176b26c755a5799be91fc0286
Suniksha <sunikshabenpatel@gmail.com>
2024-08-22 21:20:04 +0530

    Searching and Sorting Set - 2

--- 5_Searching_&_Sorting/Set-2/P04_leadersin_array.java
+++ 5_Searching_&_Sorting/Set-2/P04_leadersin_array.java
@@ -38,3 +38,22 @@ public class P04_leadersin_array {
  * Time Complexity : O(N)
  * Space Complexity : O(1)
 */
+
+public class P04_leadersin_array {
+    void printLeaders(int arr[] , int size) {
+        int max_from_right = arr[size-1];
+        System.out.print(max_from_right + " ");
+        for(int i = size-2;i>=0;i--){
+            if(max_from_right<arr[i]){
+                max_from_right = arr[i];
+                System.out.print(max_from_right+" ");
+            }
+        }
+    }
+    public static void main(String[] args) {
+        P04_leadersin_array lead = new P04_leadersin_array();
+        int arr[] = new int[]{16, 17, 4, 3, 5, 2};
+        int n = arr.length;
+        lead.printLeaders(arr, n);
+    }
+}
