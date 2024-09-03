3adbe3f1ee9a697b07823f8e12d60ffcf3fb6b37
Suniksha <sunikshabenpatel@gmail.com>
2024-09-03 14:47:35 +0530

    Linked List SET-1

--- 7_Linked_List/Set-1/P06_Delete_Middle.java
+++ 7_Linked_List/Set-1/P06_Delete_Middle.java
@@ -1,7 +1,10 @@
 /*Implement Code in java in which we have to 
 To delete a node in the middle, we must first traverse the list 
 to find the node just before the one we want to delete. 
-Then, adjust the pointers to bypass the node being deleted.*/
+Then, adjust the pointers to bypass the node being deleted.
+
+Time Complexity : O(n)
+Space Complexity : O(1)*/
 
 class Node{
     int data;
