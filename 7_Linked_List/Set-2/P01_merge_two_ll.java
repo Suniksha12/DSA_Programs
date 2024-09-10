bdb2e6f1f379be995a6471737b72a0f678a25fa8
Suniksha <sunikshabenpatel@gmail.com>
2024-09-10 21:48:26 +0530

    Linked List SET-2

--- 7_Linked_List/Set-2/P01_merge_two_ll.java
+++ 7_Linked_List/Set-2/P01_merge_two_ll.java
@@ -81,6 +81,7 @@ public class P01_merge_two_ll{
  * Space Complexity : O(m+n)
  */
 
+/*
 class Node {
     int data;
     Node next;
@@ -126,6 +127,72 @@ public class P01_merge_two_ll{
         while(temp != null) {
             System.out.print(temp.data + " ");
             temp = temp.next;
+        }
+    }
+} */
+
+/*Approach - 3 Using Iterative Merge 
+ * 
+ * Time Complexity: O(m + n),
+Auxiliary Space: O(1)
+*/
+
+public class P01_merge_two_ll{
+    static class Node{
+        int data;
+        Node next;
+        Node(int x){
+            data = x;
+            next = null;
+        }
+    }
+    static Node mergeList(Node a, Node b){
+        if(a==null) return b;
+        if(b==null) return a;
+
+        Node head = null;
+        Node tail = null;
+
+        if(a.data <= b.data) {
+            head = tail = a;
+            a= a.next;
+        }
+        else {
+            head = tail = b;
+            b = b.next;
+        }
+
+        while(a != null && b != null) {
+            if(a.data <= b.data) {
+                tail.next = a;
+                tail = a;
+                a = a.next;
+            } else {
+                tail.next = b;
+                tail = b;
+                b = b.next;
+            }
+        }
+        tail.next = (a != null) ? a:b;
+        return head;
+    }
+    public static void main(String[] args) {
+        Node a = new Node(2);
+        a.next = new Node(4);
+        a.next.next = new Node(8);
+        a.next.next.next = new Node(9);
+
+        Node b = new Node(1);
+        b.next = new Node(3);
+        b.next.next = new Node(8);
+        b.next.next.next = new Node(10);
+
+        Node res = mergeList(a, b);
+
+        System.out.println("Merged Linked List is : ");
+        while(res != null) {
+            System.out.print(res.data + " ");
+            res = res.next;
         }
     }
 }
