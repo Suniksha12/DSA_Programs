af0bdd52b61f2e011c7717b8dfc673eabd12441f
Suniksha <sunikshabenpatel@gmail.com>
2024-09-10 22:00:05 +0530

    Linked List SET-2

---
+++ 7_Linked_List/Set-2/P02_nth_node_from_end.java
@@ -0,0 +1,23 @@
+/*Implement a Code in java in which we have been Given a Linked List of M nodes and a number N,
+ find the value at the Nth node from the end of the Linked List. 
+ If there is no Nth node from the end, print -1.
+
+Examples:
+
+Input: 1 -> 2 -> 3 -> 4, N = 3
+Output: 2
+Explanation: Node 2 is the third node from the end of the linked list.
+
+Input: 35 -> 15 -> 4 -> 20, N = 4
+Output: 35
+Explanation: Node 35 is the fourth node from the end of the linked list. 
+
+Approach -1 Naive Approach 
+
+Time Complexity : O(m)
+Space Complexity : O(1)*/
+
+
+public class P02_nth_node_from_end {
+    
+}
