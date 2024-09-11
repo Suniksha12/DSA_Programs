029f16f8bfc4c001760ba0eec6b7c4550eb98260
Suniksha <sunikshabenpatel@gmail.com>
2024-09-11 16:25:40 +0530

    Linked List SET-2

--- 7_Linked_List/Set-2/P03_pairwise_swap.java
+++ 7_Linked_List/Set-2/P03_pairwise_swap.java
@@ -1,3 +1,17 @@
+/*implement Code in java in which we have been Given a singly linked list, 
+write a function to swap elements pairwise. 
+
+Input : 1->2->3->4->5->6->NULL 
+Output : 2->1->4->3->6->5->NULL
+
+Input : 1->2->3->4->5->NULL 
+Output : 2->1->4->3->5->NULL 
+
+Approach -1 Iterative
+
+Time Complexity : O(N)
+Space Complexity : O(1)*/
+
 public class P03_pairwise_swap {
     
 }
