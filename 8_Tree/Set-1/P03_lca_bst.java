1370590859578a387c359dbcb9f985d11a418dfe
Suniksha <sunikshabenpatel@gmail.com>
2024-09-19 15:40:17 +0530

    Tree Set-1

--- 8_Tree/Set-1/P03_lca_bst.java
+++ 8_Tree/Set-1/P03_lca_bst.java
@@ -70,3 +70,57 @@ class P03_lca_bst {
     }
 }
 */
+
+/*Approach - 2 Iterative Method Using BST 
+ * 
+ * Time Compelxity : O(h)
+ * Space Complexity : O(1)
+ */
+
+class Node{
+    int data;
+    Node left , right;
+    Node(int item){
+        data = item;
+        left = right = null;
+    }
+}
+class P03_lca_bst{
+    Node root;
+    static Node lca(Node root, int n1, int n2){
+        while(root != null){
+            if(root.data > n1 && root.data>n2) root=root.left;
+            else if(root.data<n1 &&root.data<n2) root = root.right;
+            else 
+               break;
+        }
+        return root;
+    }
+    public static void main(String[] args) {
+        P03_lca_bst tree = new P03_lca_bst();
+        tree.root = new Node(20);
+        tree.root.left = new Node(8);
+        tree.root.right = new Node(22);
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(12);
+        tree.root.left.right.left = new Node(10);
+        tree.root.left.right.right = new Node(14);
+
+        int n1 = 10,n2=14;
+        Node t = tree.lca(tree.root, n1, n2);
+        System.out.println("LCA of " + n1 + " and " + n2
+                           + " is " + t.data);
+
+        n1 = 14;
+        n2 = 8;
+        t = tree.lca(tree.root, n1, n2);
+        System.out.println("LCA of " + n1 + " and " + n2
+                           + " is " + t.data);
+
+        n1 = 10;
+        n2 = 22;
+        t = tree.lca(tree.root, n1, n2);
+        System.out.println("LCA of " + n1 + " and " + n2
+                           + " is " + t.data);
+    }
+}
