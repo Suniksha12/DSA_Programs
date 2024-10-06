df6e680577e4b300485a96258a59c41cc9771f93
Suniksha <sunikshabenpatel@gmail.com>
2024-10-06 15:56:51 +0530

    Dynamic Programming Set-1

--- 11_Dynamic_Programming/Set-1/P01_Longest_increasing_subsequence.java
+++ 11_Dynamic_Programming/Set-1/P01_Longest_increasing_subsequence.java
@@ -43,6 +43,7 @@ class P01_Longest_increasing_subsequence {
  * Space Complexity : O(N2)
  */
 
+/*
 class P01_Longest_increasing_subsequence {
     static int lisEndAtI(int arr[], int i, int dp[]) {
         if (i == 0)
@@ -74,5 +75,34 @@ class P01_Longest_increasing_subsequence {
         int arr[] = { 10, 22, 9, 33, 21, 50, 41, 60 };
         int n = arr.length;
         System.out.println("Length of lis is " + lis(arr, n));
+    }
+} */
+
+/*Approach - 3 Dynamic Programming
+ * 
+ * Time Complexity : O(N^2)
+ * Space Complexity : O(N)
+ */
+
+class P01_Longest_increasing_subsequence {
+    static int lis(int arr[], int n) {
+        int lis[] = new int[n];
+        for (int i = 0; i < n; i++)
+            lis[i] = 1;
+        for (int i = 1; i < n; i++)
+            for (int prev = 0; prev < i; prev++)
+                if (arr[i] > arr[prev] && lis[i] < lis[prev] + 1)
+                    lis[i] = lis[prev] + 1;
+        int max = 1;
+        for (int i = 1; i < n; i++)
+            max = Math.max(max, lis[i]);
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 10, 22, 9, 33, 21, 50, 41, 60 };
+        int n = arr.length;
+        System.out.println("Length of lis is "
+                + lis(arr, n));
     }
 }
