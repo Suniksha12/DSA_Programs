7ce8fd367a695f8e33995f71f81ac0157be6f3be
Suniksha <sunikshabenpatel@gmail.com>
2024-10-07 15:10:21 +0530

    Dynamic Programming Set-1

--- 11_Dynamic_Programming/Set-1/P05_Longest_Common_subsequence.java
+++ 11_Dynamic_Programming/Set-1/P05_Longest_Common_subsequence.java
@@ -37,4 +37,39 @@ public class P05_Longest_Common_subsequence {
     }
 }*/
 
+/*Aprroach -2 Memoization 
+ * 
+ * Time Complexity : O(m*n)
+ * Space complexity : O(m*n)
+ */
 
+import java.util.Arrays;
+
+public class P05_Longest_Common_subsequence {
+    static int lcs(String s1, String s2, int m, int n, int[][] memo) {
+        if (m == 0 || n == 0)
+            return 0;
+        if (memo[m][n] != -1)
+            return memo[m][n];
+        if (s1.charAt(m - 1) == s2.charAt(n - 1)) {
+            return memo[m][n] = 1 + lcs(s1, s2, m - 1, n - 1, memo);
+        }
+        return memo[m][n] = Math.max(lcs(s1, s2, m, n - 1, memo), lcs(s1, s2, m - 1, n, memo));
+    }
+
+    public static void main(String[] args) {
+        String S1 = "AGGTAB";
+        String S2 = "GXTXAYB";
+
+        int m = S1.length();
+        int n = S2.length();
+        int[][] memo = new int[m + 1][n + 1];
+
+        for (int i = 0; i <= m; i++) {
+            Arrays.fill(memo[i], -1);
+        }
+
+        System.out.println("Length of LCS is "
+                + lcs(S1, S2, m, n, memo));
+    }
+}
