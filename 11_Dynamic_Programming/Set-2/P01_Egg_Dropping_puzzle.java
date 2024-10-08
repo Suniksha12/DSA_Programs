86d5b8055556df8b61447917cfb663c90854b977
Suniksha <sunikshabenpatel@gmail.com>
2024-10-08 12:44:40 +0530

    Dynamic Programming Set-2

--- 11_Dynamic_Programming/Set-2/P01_Egg_Dropping_puzzle.java
+++ 11_Dynamic_Programming/Set-2/P01_Egg_Dropping_puzzle.java
@@ -16,6 +16,7 @@ Approach - 1 Using Recursion
 Time Complexity : Exponential
 Space Complexity : O(1)*/
 
+/* 
 public class P01_Egg_Dropping_puzzle {
     static int eggDrop(int n , int k){
         if(k==0 || k==1)
@@ -40,3 +41,47 @@ public class P01_Egg_Dropping_puzzle {
                          + eggDrop(n, k));
     }
 }
+*/
+
+/*Approach - 2 Dynamic Programming
+ * 
+ * Time Complexity : O(N*k^2)
+ * Space Complexity : O(N*k^2)
+ */
+
+ public class P01_Egg_Dropping_puzzle {
+    static int max(int a , int b){
+        return (a>b)?a:b;
+    } 
+    static int eggDrop(int n , int k){
+        int eggFloor[][] = new int[n+1][k+1];
+        int res;
+        int i, j,x;
+
+        for(i=1;i<=n;i++){
+            eggFloor[i][1] = 1;
+            eggFloor[i][0] = 0;
+        }
+        for(j=1;j<=k;j++)
+           eggFloor[1][j] = j;
+
+        for(i=2;i<=n;i++){
+            for(j=2;j<=k;j++){
+                eggFloor[i][j] = Integer.MAX_VALUE;
+                for(x=1;x<=j;x++){
+                    res=1 + Math.max(eggFloor[i-1][x-1],eggFloor[i][j-x]);
+                    if(res < eggFloor[i][j])
+                       eggFloor[i][j] = res;
+                }
+            }
+        }
+        return eggFloor[n][k];
+    }
+    public static void main(String[] args) {
+        int n = 2, k = 36;
+        System.out.println(
+            "Minimum number of trials in worst"
+            + " case with " + n + "  eggs and " + k
+            + " floors is " + eggDrop(n, k));       
+    }
+ }
