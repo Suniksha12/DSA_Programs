4d2bd7c954dbcfd84d5ef803fb40b43b0ea7bb62
Suniksha <sunikshabenpatel@gmail.com>
2024-10-13 09:41:51 +0530

    Heap_and_hashing Set-2

--- 13_Heap_&_Hashing/Set-2/P01_smallest_dearrangement_sequence.java
+++ 13_Heap_&_Hashing/Set-2/P01_smallest_dearrangement_sequence.java
@@ -66,6 +66,25 @@ public class P01_smallest_dearrangement_sequence {
  */
 
  public class P01_smallest_dearrangement_sequence {
- 
-    
+      static void generate_derangement(int N){
+        int S[] = new int[N+1];
+        for(int i=1;i<=N;i++)
+           S[i] = i;
+        int D[] = new int[N+1];
+        for(int i=1;i<=N;i+=2){
+            if(i==N){
+                D[N] = S[N-1];
+                D[N-1]= S[N];
+            } else {
+                D[i] = i+1;
+                D[i+1] = i;
+            }
+        }
+        for (int i = 1; i <= N; i++) 
+        System.out.print(D[i] + " "); 
+    System.out.println(); 
+      }
+      public static void main(String[] args) {
+        generate_derangement(10);
+      }
  }
